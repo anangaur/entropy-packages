dabde0a6fa0460dd10eee1bb0d44949f9aedf8f1
Anand Gaurav <cs.anandgaurav@gmail.com>
2019-09-09 12:48:54 -0700

    init

---
+++ my.sample.lib/EntropyLib.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace my.sample.lib
+{
+    public class Class1
+    {
+        public static string getText()
+        {
+            return "Hello World from My.Sample.Lib v 0.0.1";
+        }
+    }
+}
